aab334fe4ec4de1193d3e428be4d3276d7a926ea
hatone <b1006006@gmail.com>
2017-01-23 22:22:38 -0800

    add readNumber function

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -3,11 +3,13 @@ package lexer
 import (
 	"testing"
 
+	"fmt"
 	"monkey/token"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `let five = 5;
+	input := `
+	let five = 5;
 let ten = 10;
 
 let add = fn(x,y) {
@@ -62,13 +64,15 @@ let resut = add(five, ten);
 
 	for i, tt := range tests {
 		tok := l.NextToken()
-
+		fmt.Println(tok.Literal)
 		if tok.Type != tt.expectedType {
+			fmt.Println(tok.Literal)
 			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
 				i, tt.expectedType, tok.Type)
 		}
 
 		if tok.Literal != tt.expectedLiteral {
+			fmt.Println(tok.Literal)
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
 				i, tt.expectedLiteral, tok.Literal)
 		}
